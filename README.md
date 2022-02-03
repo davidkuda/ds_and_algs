40ccc75f19386e575483de62778cb2165cd2d015
David Kuda <david.kuda@pm.me>
2022-02-03 22:43:05 +0100

    Setup basic maven project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Algorithms and Data Structures
+
+I use this repository to code along with:
+
+- __CCI:__ https://www.crackingthecodinginterview.com/
+- __ALGS4:__ https://algs4.cs.princeton.edu/home/
---
+++ src/main/java/com/davidkuda/algs/App.java
@@ -0,0 +1,13 @@
+package com.davidkuda.algs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
