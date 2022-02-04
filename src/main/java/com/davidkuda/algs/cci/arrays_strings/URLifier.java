3459c0c5024fab3bea6345c7bbea1a6eec23d84d
David Kuda <david.kuda@pm.me>
2022-02-04 22:03:45 +0100

    feat: Solve problem "URLify"

---
+++ src/main/java/com/davidkuda/algs/cci/arrays_strings/URLifier.java
@@ -0,0 +1,35 @@
+package com.davidkuda.algs.cci.arrays_strings;
+
+public class URLifier {
+
+  /**
+   * Iterate over a String and replace all spaces with "%20".
+   * @param s The String as input
+   * @param len The length of the eventual CharArray
+   * @return String
+   */
+  public String urlify(String s, int len) {
+    char[] charArr = new char[len];
+
+    int counter = 0;
+    for (char c : s.toCharArray()) {
+
+      if (c == ' ') {
+        charArr[counter] = '%';
+        counter++;
+        charArr[counter] = '2';
+        counter++;
+        charArr[counter] = '0';
+        counter++;
+        continue;
+      }
+
+      charArr[counter] = c;
+      counter++;
+    }
+
+    String sTransformed = new String(charArr);
+    System.out.println("sTransformed: " + sTransformed);
+    return sTransformed;
+  }
+}
