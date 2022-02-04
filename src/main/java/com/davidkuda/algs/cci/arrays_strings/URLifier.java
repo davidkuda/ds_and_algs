167bd291ed4b7a0b66593a54c514bc97e19675f4
David Kuda <david.kuda@pm.me>
2022-02-04 22:25:26 +0100

    Find out len of charArray from len of str

--- src/main/java/com/davidkuda/algs/cci/arrays_strings/URLifier.java
+++ src/main/java/com/davidkuda/algs/cci/arrays_strings/URLifier.java
@@ -8,8 +8,12 @@ public class URLifier {
    * @param len The length of the eventual CharArray
    * @return String
    */
-  public String urlify(String s, int len) {
-    char[] charArr = new char[len];
+  public String urlify(String s) {
+
+    int numOfSpaces = countNumOfChar(s, ' ');
+    int newLen = s.length() + numOfSpaces * 2;
+
+    char[] charArr = new char[newLen];
 
     int counter = 0;
     for (char c : s.toCharArray()) {
@@ -31,5 +35,15 @@ public class URLifier {
     String sTransformed = new String(charArr);
     System.out.println("sTransformed: " + sTransformed);
     return sTransformed;
+  }
+
+  public int countNumOfChar(String s, char targetChar) {
+    int counter = 0;
+    for (char c : s.toCharArray()) {
+      if (c == targetChar)  {
+        counter++;
+      }
+    }
+    return counter;
   }
 }
--- src/test/java/com/davidkuda/algs/cci/arrays_strings/URLifierTest.java
+++ src/test/java/com/davidkuda/algs/cci/arrays_strings/URLifierTest.java
@@ -8,7 +8,7 @@ public class URLifierTest {
   @Test
   public void testUrlifier() {
     URLifier u = new URLifier();
-    String actual = u.urlify("This Love Has Taken its Toll On Me", 48);
+    String actual = u.urlify("This Love Has Taken its Toll On Me");
     String expected = "This%20Love%20Has%20Taken%20its%20Toll%20On%20Me";
     Assert.assertEquals(expected, actual);
   }
@@ -16,7 +16,7 @@ public class URLifierTest {
   @Test
   public void testUrlifierSimple() {
     URLifier u = new URLifier();
-    String actual = u.urlify("Hello World", 13);
+    String actual = u.urlify("Hello World");
     String expected = "Hello%20World";
     Assert.assertEquals(expected, actual);
   }
