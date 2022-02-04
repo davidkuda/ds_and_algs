88e6192ccda8e496c2fe1899921630fce50b85a6
David Kuda <david.kuda@pm.me>
2022-02-04 20:54:04 +0100

    test: Create a simple test case for "URLifier"

---
+++ src/test/java/com/davidkuda/algs/cci/arrays_strings/URLifierTest.java
@@ -0,0 +1,16 @@
+package com.davidkuda.algs.cci.arrays_strings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class URLifierTest {
+  
+  @Test
+  public void testUrlifier() {
+    URLifier u = new URLifier();
+    String actual = u.urlify("This Love Has Taken its Toll On Me", 48);
+    String expected = "This%20Love%20Has%20Taken%20its%20Toll%20On%20Me";
+    Assert.assertEquals(expected, actual);
+  }
+  
+}
