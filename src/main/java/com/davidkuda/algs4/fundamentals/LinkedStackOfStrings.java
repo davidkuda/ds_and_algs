1ce85cc607cbd8c12b596da67c04154c7d362f36
David Kuda <david.kuda@pm.me>
2022-02-05 21:11:14 +0100

    feat: Define DS for "LinkedStackOfStrings"

---
+++ src/main/java/com/davidkuda/algs4/fundamentals/LinkedStackOfStrings.java
@@ -0,0 +1,28 @@
+package com.davidkuda.algs4.fundamentals;
+
+public class LinkedStackOfStrings {
+
+  private Node top;
+
+  private class Node {
+    String item;
+    Node next;
+  }
+
+  public boolean isEmpty() {
+    return top == null;
+  }
+
+  public void push(String element) {
+    Node newNode = new Node();
+    newNode.item = element;
+    newNode.next = top;
+    top = newNode;
+  }
+
+  public String pop() {
+    String topValue = top.item;
+    top = top.next;
+    return topValue;
+  } 
+}
