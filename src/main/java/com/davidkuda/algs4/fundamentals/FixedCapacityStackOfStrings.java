bbe3de13aeba3910a09c11d056df1b92e9287d7f
David Kuda <david.kuda@pm.me>
2022-02-05 21:11:33 +0100

    feat: Define DS for "FixedCapacityStackOfStrings"

---
+++ src/main/java/com/davidkuda/algs4/fundamentals/FixedCapacityStackOfStrings.java
@@ -0,0 +1,23 @@
+package com.davidkuda.algs4.fundamentals;
+
+public class FixedCapacityStackOfStrings {
+
+  private String[] stack;
+  private int N = 0;
+
+  public FixedCapacityStackOfStrings(int capacity) {
+    stack = new String[capacity];
+  }
+
+  public boolean isEmpty() {
+    return N == 0;
+  }
+  
+  public void push(String element) {
+    stack[N++] = element;
+  }
+
+  public String pop() {
+    return stack[--N];   
+  }
+}
