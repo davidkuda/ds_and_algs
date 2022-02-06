5f4109d968a10311ede9875e2e776a0ab09c0b7e
David Kuda <david.kuda@pm.me>
2022-02-06 17:25:33 +0100

    test: Test resizing of arrayQ with further enq and deq

--- src/test/java/com/davidkuda/algs4/fundamentals/QueueTests.java
+++ src/test/java/com/davidkuda/algs4/fundamentals/QueueTests.java
@@ -32,10 +32,22 @@ public class QueueTests {
     queue.enqueue("First");
     queue.enqueue("Second");
     queue.enqueue("Third");
+    queue.enqueue("Fourth");
+    queue.enqueue("Fifth");
+    queue.enqueue("Sixth");
+    queue.enqueue("Seventh");
+    queue.enqueue("Eigth");
+    queue.enqueue("Ninth");
 
     // Then:
     Assert.assertEquals("First", queue.dequeue());
     Assert.assertEquals("Second", queue.dequeue());
     Assert.assertEquals("Third", queue.dequeue());
+    Assert.assertEquals("Fourth", queue.dequeue());
+    Assert.assertEquals("Fifth", queue.dequeue());
+    Assert.assertEquals("Sixth", queue.dequeue());
+    Assert.assertEquals("Seventh", queue.dequeue());
+    Assert.assertEquals("Eigth", queue.dequeue());
+    Assert.assertEquals("Ninth", queue.dequeue());
   }
 }
