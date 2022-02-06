b42895795d4d5de2e5f42a240161d2f997e2afaf
David Kuda <david.kuda@pm.me>
2022-02-06 17:03:40 +0100

    test: Create test for LinkedQueue

---
+++ src/test/java/com/davidkuda/algs4/fundamentals/QueueTests.java
@@ -0,0 +1,23 @@
+package com.davidkuda.algs4.fundamentals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QueueTests {
+
+  @Test
+  public void testLinkedQueueOfStrings() {
+    // Given:
+    LinkedQueueOfStrings queue = new LinkedQueueOfStrings();
+
+    // When:
+    queue.enqueue("First");
+    queue.enqueue("Second");
+    queue.enqueue("Third");
+
+    // Then:
+    Assert.assertEquals("First", queue.dequeue());
+    Assert.assertEquals("Second", queue.dequeue());
+    Assert.assertEquals("Third", queue.dequeue());
+  }
+}
