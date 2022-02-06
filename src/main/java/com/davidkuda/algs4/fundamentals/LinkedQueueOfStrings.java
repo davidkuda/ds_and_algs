64675a8d01d09465adb94044094486877d96e49e
David Kuda <david.kuda@pm.me>
2022-02-06 17:04:12 +0100

    feat: Define DS for "LinkedQueueOfStrings"

---
+++ src/main/java/com/davidkuda/algs4/fundamentals/LinkedQueueOfStrings.java
@@ -0,0 +1,49 @@
+package com.davidkuda.algs4.fundamentals;
+
+public class LinkedQueueOfStrings {
+
+  private Node first;
+  private Node last;
+  private int size;
+
+  // Same as in Queue:
+  private class Node {
+    String item;
+    Node next;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return first == null;
+  }
+
+  public void enqueue(String item) {
+    Node oldLast = last;
+
+    last = new Node();
+    last.item = item;
+    last.next = null;
+
+    if (isEmpty()) {
+      first = last;
+    } else  {
+      oldLast.next = last;
+    }
+  }
+
+  // Queue.dequeue() is identical to Stack.pop()
+  public String dequeue() {
+    String item = first.item;
+    first = first.next;
+    size--;
+
+    if (isEmpty()) {
+      last = null;
+    }
+
+    return item;
+  }
+}
