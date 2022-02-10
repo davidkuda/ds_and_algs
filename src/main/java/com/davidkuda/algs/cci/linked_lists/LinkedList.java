0f919ed2bc6b94b8cf4d9453c022c02db12884bd
David Kuda <david.kuda@pm.me>
2022-02-10 23:32:37 +0100

    Implement simple DS for LinkedList

---
+++ src/main/java/com/davidkuda/algs/cci/linked_lists/LinkedList.java
@@ -0,0 +1,63 @@
+package com.davidkuda.algs.cci.linked_lists;
+
+public class LinkedList<Item> {
+
+  private Node first;
+  private Node last;
+  private int size;
+
+  private class Node {
+    Item item;
+    Node next;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public void insertAtBeginning(Item item) {
+    if (isEmpty()) {
+      // Note: This check is only required if there is the field "last".
+      first = new Node();
+      first.item = item;
+      last = first;
+      size = 1;
+    }
+
+    Node oldFirst = first;
+    first = new Node();
+    first.item = item;
+    first.next = oldFirst;
+    size++;
+  }
+
+  public void removeFromBeginning() {
+    // "first" becomes an orphan; Java memory management will reclaim the space
+    // ("Garbage Collection")
+    first = first.next;
+    size--;
+
+    if (isEmpty()) {
+      last = null;
+    }
+  }
+
+  public void insertAtEnd(Item item) {
+    Node oldLast = last;
+    last = new Node();
+    last.item = item;
+    oldLast.next = last;
+    size++;
+  }
+
+  private void traverse() {
+    for (Node x = first; x != null; x = x.next) {
+      System.out.println(x.item);
+    }
+  }
+  
+}
