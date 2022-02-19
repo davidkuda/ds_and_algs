8ec084905f5539192b9fdbee8ee4991930f10b3c
David Kuda <david.kuda@pm.me>
2022-02-19 17:00:37 +0100

    test: Create "testIteration()"

--- src/test/java/com/davidkuda/algs/cci/linked_lists/LinkedListTest.java
+++ src/test/java/com/davidkuda/algs/cci/linked_lists/LinkedListTest.java
@@ -6,6 +6,33 @@ import org.junit.Test;
 public class LinkedListTest {
 
   @Test
+  public void testGetSize() {
+    LinkedList<Integer> l = new LinkedList<Integer>();    
+
+    Assert.assertEquals(0, l.getSize());
+
+    l.insertAtBeginning(108);
+
+    Assert.assertEquals(1, l.getSize());
+
+    l.insertAtBeginning(108);
+    Assert.assertEquals(2, l.getSize());
+
+    l.insertAtBeginning(108);
+    Assert.assertEquals(3, l.getSize());
+    
+
+    l.removeFromBeginning();
+    Assert.assertEquals(2, l.getSize());
+
+    l.removeFromBeginning();
+    Assert.assertEquals(1, l.getSize());
+
+    l.removeFromBeginning();
+    Assert.assertEquals(0, l.getSize());
+  }
+
+  @Test
   public void testRemoveDups() {
 
     // given:
@@ -30,6 +57,22 @@ public class LinkedListTest {
     Assert.assertEquals("Third", l.first.next.next.item);
     Assert.assertEquals("Fourth", l.first.next.next.next.item);
     Assert.assertEquals("Fifth", l.first.next.next.next.next.item);
+  }
+
+  @Test
+  public void testIteration() {
+    // given:
+    LinkedList<String> l = new LinkedList<String>();
+
+    // when:
+    l.insertAtBeginning("First");
+    l.insertAtEnd("Second");
+    l.insertAtEnd("Third");
+    
+    // then:
+    for (String i : l) {
+      System.out.println(i);
+    }
   }
   
 }
