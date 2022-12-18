763ac01b66ed71f98b3565b60cd4ac85a6ad6497
David Kuda <david@kuda.ai>
2022-12-18 21:30:05 +0100

    feat: implement linked queue in go

---
+++ go/algs4/queue/queue_test.go
@@ -0,0 +1,20 @@
+package queue
+
+import "testing"
+
+func TestQueue(t *testing.T) {
+	expected := [3]string{"first", "second", "third"}
+	q := Queue{}
+	q.Enqueue("first")
+	q.Enqueue("second")
+	q.Enqueue("third")
+
+	var actual [3]string
+	for i := range actual {
+		actual[i] = q.Dequeue()
+	}
+
+	if actual != expected {
+		t.Errorf("Something wrong!\nexpected: %v\nactual: %v", expected, actual)
+	}
+}
