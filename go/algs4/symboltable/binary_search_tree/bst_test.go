a949907592097267b772158a7950a6de9142593d
davidkuda <david@kuda.ai>
2022-12-25 17:23:20 +0100

    test: test bst.Keys()

--- go/algs4/symboltable/binary_search_tree/bst_test.go
+++ go/algs4/symboltable/binary_search_tree/bst_test.go
@@ -10,8 +10,6 @@ func TestPut(t *testing.T) {
 	bst.Put("alpha", 108)
 	bst.Put("bravo", 108)
 	bst.Put("charlie", 108)
-	bst.Put("sierra", 108)
-	bst.Put("romeo", 108)
 }
 
 func TestMin(t *testing.T) {
@@ -46,6 +44,24 @@ func TestFloor(t *testing.T) {
 	k3, _ := bst.Floor("tango")
 	if k3 != "sierra" {
 		t.Errorf("Floor was not succesful. Expected: charlie; Received: %s", k3)
+	}
+}
+
+func TestKeys(t *testing.T) {
+	var sum int
+	bst := prepareBst()
+	keysQueue := bst.Keys()
+	for {
+		key, ok := keysQueue.Dequeue()
+		if !ok {
+			break
+		}
+		v, _ := bst.get(key)
+		sum += v
+	}
+	expected := 42 + 6*108
+	if sum != expected {
+		t.Errorf("Did you get the right keys? Expected: %d, Actual %d", expected, sum)
 	}
 }
 
