8ba18a199a2db48887807bc3ea33ffee3ebe3e0b
davidkuda <david@kuda.ai>
2022-12-26 21:05:17 +0100

    test: use a second way to iterate a queue

--- go/algs4/symboltable/binary_search_tree/queue_test.go
+++ go/algs4/symboltable/binary_search_tree/queue_test.go
@@ -22,3 +22,23 @@ func TestQueue(t *testing.T) {
 		t.Errorf("Something wrong!\nexpected: %v\nactual: %v", expected, actual)
 	}
 }
+
+func TestQueueIteration(t *testing.T) {
+	expected := [3]string{"first", "second", "third"}
+	q := NewQueue[string]()
+	q.Enqueue("first")
+	q.Enqueue("second")
+	q.Enqueue("third")
+
+	var actual [3]string
+	var count int
+	for i, ok := q.Dequeue(); ok != false; i, ok = q.Dequeue() {
+		t.Log(i)
+		actual[count] = i
+		count++
+	}
+
+	if actual != expected {
+		t.Errorf("Something wrong!\nexpected: %v\nactual: %v", expected, actual)
+	}
+}
