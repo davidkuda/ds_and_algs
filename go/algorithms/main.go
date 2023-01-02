2485dbd918e253efd881d537641a92e7e3aa45ac
davidkuda <david@kuda.ai>
2023-01-02 19:09:01 +0100

    style: apply go fmt

--- go/algorithms/main.go
+++ go/algorithms/main.go
@@ -1,42 +1,44 @@
 package main
 
- type ListNode struct {
-    Val int
-    Next *ListNode
- }
+// --- LinkedLists
 
- // --- reverse linked list
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
- // reverse by creating a new Node
- func reverseList1(head *ListNode) *ListNode {
-    var n *ListNode
-    for head != nil {
-        n = &ListNode{Val: head.Val, Next: n}
-        head = head.Next
-    }
-    return n
+// --- reverse linked list
+
+// reverse by creating a new Node
+func reverseList1(head *ListNode) *ListNode {
+	var n *ListNode
+	for head != nil {
+		n = &ListNode{Val: head.Val, Next: n}
+		head = head.Next
+	}
+	return n
 }
 
 // reverse by storing head as tempNext
 func reverseList2(head *ListNode) *ListNode {
-    var r *ListNode
-    for head != nil {
-        tempNext := head.Next
-        head.Next = r
-        r = head
-        head = tempNext
-    }
-    return r
+	var r *ListNode
+	for head != nil {
+		tempNext := head.Next
+		head.Next = r
+		r = head
+		head = tempNext
+	}
+	return r
 }
 
 func middleNode(head *ListNode) *ListNode {
-    mid := head
-    for head != nil {
-        if head.Next == nil {
-            break
-        }
-        mid = mid.Next
-        head = head.Next.Next
-    }
-    return mid
+	mid := head
+	for head != nil {
+		if head.Next == nil {
+			break
+		}
+		mid = mid.Next
+		head = head.Next.Next
+	}
+	return mid
 }
