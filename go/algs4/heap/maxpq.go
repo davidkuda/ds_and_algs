1e1b9bc059c4e3d681443e0129a68d5dab603c4a
davidkuda <david@kuda.ai>
2023-01-03 15:52:58 +0100

    feat: implement a constructor for the MaxPQ

--- go/algs4/heap/maxpq.go
+++ go/algs4/heap/maxpq.go
@@ -8,6 +8,14 @@ type MaxPriorityQueue[T constraints.Ordered] struct {
 	items []T
 }
 
+// use this constructor if you know the number of elements that the pq should hold.
+// otherwise, the slice may need to be resized.
+func NewMaxPriorityQueue[T constraints.Ordered](size int) *MaxPriorityQueue[T] {
+	return &MaxPriorityQueue[T]{
+		items: make([]T, size),
+	}
+}
+
 func (pq *MaxPriorityQueue[T]) Size() int {
 	return len(pq.items)
 }
