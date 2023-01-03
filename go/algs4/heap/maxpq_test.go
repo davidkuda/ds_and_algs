e7f77c40d8ed0a145cd59ecd202c7f7700860e64
davidkuda <david@kuda.ai>
2023-01-03 18:30:34 +0100

    test: write tests for MaxPQ

--- go/algs4/heap/maxpq_test.go
+++ go/algs4/heap/maxpq_test.go
@@ -3,5 +3,20 @@ package heap
 import "testing"
 
 func TestMaxPQ(t *testing.T) {
-	t.Log("Hello World")
+	pq := MaxPriorityQueue[int]{}
+
+	values := []int{10, 7, 12, 20, 5, 3, 21}
+
+	for _, v := range values {
+		pq.Insert(v)
+	}
+
+	if pq.Size() != 7 {
+		t.Errorf("Expected size of 7, but got %d", pq.Size())
+	}
+	max := pq.DelMax()
+
+	if max != 21 {
+		t.Errorf("Expected max to be 21, but was %d", max)
+	}
 }
