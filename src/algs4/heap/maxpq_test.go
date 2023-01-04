36e27900055411e467ecb8c82029e301b9795a04
davidkuda <david@kuda.ai>
2023-01-05 00:07:54 +0100

    update tests

--- src/algs4/heap/maxpq_test.go
+++ src/algs4/heap/maxpq_test.go
@@ -14,7 +14,10 @@ func TestMaxPQ(t *testing.T) {
 	if pq.Size() != 7 {
 		t.Errorf("Expected size of 7, but got %d", pq.Size())
 	}
-	max := pq.DelMax()
+	max, ok := pq.Poll()
+	if !ok {
+		t.Errorf("Tried to poll from empty pq?")
+	}
 
 	if max != 21 {
 		t.Errorf("Expected max to be 21, but was %d", max)
@@ -23,19 +26,19 @@ func TestMaxPQ(t *testing.T) {
 
 func TestParent(t *testing.T) {
 	
-	if parent(0) != 0 {
-		t.Errorf("Failed to get parent, exp 0, got %d", parent(0))
+	if parentIndex(0) != 0 {
+		t.Errorf("Failed to get parentIndex, exp 0, got %d", parentIndex(0))
 	}
 
-	if parent(1) != 0 {
-		t.Errorf("Failed to get parent, exp 0, got %d", parent(1))
+	if parentIndex(1) != 0 {
+		t.Errorf("Failed to get parentIndex, exp 0, got %d", parentIndex(1))
 	}
 
-	if parent(2) != 0 {
-		t.Errorf("Failed to get parent, exp 0, got %d", parent(2))
+	if parentIndex(2) != 0 {
+		t.Errorf("Failed to get parentIndex, exp 0, got %d", parentIndex(2))
 	}
 
-	if parent(3) != 1 {
-		t.Errorf("Failed to get parent, exp 0, got %d", parent(3))
+	if parentIndex(3) != 1 {
+		t.Errorf("Failed to get parentIndex, exp 0, got %d", parentIndex(3))
 	}
 }
