949afad90ac92a6383f18d9c870d05b4752867fb
davidkuda <david@kuda.ai>
2023-01-08 23:38:20 +0100

    test: write a test for QuickSort

--- src/algorithms/algos_test.go
+++ src/algorithms/algos_test.go
@@ -53,3 +53,15 @@ func TestShuffle(t *testing.T) {
 		t.Errorf("Nums were not sorted correctly;\nexpected: %v\nactual: %v", notExpected, nums)
 	}
 }
+
+func TestQuickSort(t *testing.T) {
+	nums := []int{10, 21, 7, 5, 3, 12, 8}
+	QuickSort(nums)
+	expected := []int{3, 5, 7, 8, 10, 12, 21}
+	for i := range nums {
+		if nums[i] != expected[i] {
+			t.Errorf("Nums were not sorted correctly;\nexpected: %v\nactual: %v", expected, nums)
+			break
+		}
+	}
+}
