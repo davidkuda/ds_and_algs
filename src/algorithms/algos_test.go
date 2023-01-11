e34eb49ed734e27b554bf93b450d60e921163e9e
davidkuda <david@kuda.ai>
2023-01-12 00:34:37 +0100

    test: create a second test for QuickSort

--- src/algorithms/algos_test.go
+++ src/algorithms/algos_test.go
@@ -97,6 +97,18 @@ func TestQuickSort(t *testing.T) {
 	}
 }
 
+func TestQuickSort2(t *testing.T) {
+	nums := []int{10, 21, 7, 5, 3, 12, 8}
+	QuickSort(nums)
+	expected := []int{3, 5, 7, 8, 10, 12, 21}
+	for i := range nums {
+		if nums[i] != expected[i] {
+			t.Errorf("Nums were not sorted correctly;\nexpected: %v\nactual: %v", expected, nums)
+			break
+		}
+	}
+}
+
 func TestQuickSelectMedian(t *testing.T) {
 	nums := []int{10, 21, 7, 5, 3, 12, 8}
 	median := QuickSelect(nums, len(nums)/2)
