502a9268dd4ab08fa46e1f5e19d15f5e98a206cd
David O. Kuda <david@kuda.ai>
2024-06-01 22:03:14 +0200

    try rod cutting without iteration

--- algorithms/dynamic_programming/rod_cutting.go
+++ algorithms/dynamic_programming/rod_cutting.go
@@ -10,6 +10,7 @@ const MaxInt = int(MaxUint >> 1)
 const MinInt = -MaxInt - 1
 
 func CutRod(prices []int, rodLength int) int {
+	fmt.Println("iterative:", rodLength)
 	if rodLength == 0 {
 		return 0
 	}
@@ -17,9 +18,28 @@ func CutRod(prices []int, rodLength int) int {
 	highestPrice := prices[rodLength]
 
 	for i := 0; i < rodLength; i++ {
-		fmt.Println(i)
 		highestPrice = max(highestPrice, prices[i]+CutRod(prices, rodLength-i-1))
 	}
 
 	return highestPrice
 }
+
+func CutRod2(p []int, n int) int {
+	fmt.Println("recursive:", n)
+	if n == 0 {
+		return 0
+	}
+
+	q := MinInt
+
+	q = max(q, p[n]+CutRod2(p, n-1))
+
+	if n > 0 {
+		q = max(q, p[n]+CutRod2(p, n-1))
+	}
+
+	fmt.Println(q)
+
+	return q
+}
+
--- algorithms/dynamic_programming/rod_cutting_test.go
+++ algorithms/dynamic_programming/rod_cutting_test.go
@@ -1,10 +1,12 @@
 package dp
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCutRod(t *testing.T) {
+	fmt.Println("Iterative:")
 	prices := []int{1, 5, 8, 9, 10, 17, 20, 24, 30}
 
 	l := 4
@@ -13,8 +15,19 @@ func TestCutRod(t *testing.T) {
 
 	want := 13
 
+	fmt.Println()
 	if have != want {
 		t.Fatalf("Failed: Got %v, Want %v", have, want)
 	}
+}
 
+func TestCutRod2(t *testing.T) {
+	fmt.Println("Recursive:")
+	prices := []int{1, 5, 8, 9, 10, 17, 20, 24, 30}
+	have := CutRod2(prices, 4)
+	want := 13
+
+	if have != want {
+		t.Fatalf("Failed: Got %v, Want %v", have, want)
+	}
 }
