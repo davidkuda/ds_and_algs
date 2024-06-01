36e6515b96059f92a387d4f7a7dae3a22136ca7a
David O. Kuda <david@kuda.ai>
2024-06-01 22:04:32 +0200

    use simpler variables in iterative version

--- algorithms/dynamic_programming/rod_cutting.go
+++ algorithms/dynamic_programming/rod_cutting.go
@@ -9,19 +9,19 @@ const MinUint = 0
 const MaxInt = int(MaxUint >> 1)
 const MinInt = -MaxInt - 1
 
-func CutRod(prices []int, rodLength int) int {
-	fmt.Println("iterative:", rodLength)
-	if rodLength == 0 {
+func CutRod(p []int, n int) int {
+	fmt.Println("iterative:", n)
+	if n == 0 {
 		return 0
 	}
 
-	highestPrice := prices[rodLength]
+	q := p[n]
 
-	for i := 0; i < rodLength; i++ {
-		highestPrice = max(highestPrice, prices[i]+CutRod(prices, rodLength-i-1))
+	for i := 0; i < n; i++ {
+		q = max(q, p[i]+CutRod(p, n-i-1))
 	}
 
-	return highestPrice
+	return q
 }
 
 func CutRod2(p []int, n int) int {
