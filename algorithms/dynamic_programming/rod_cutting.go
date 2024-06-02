23b24ba033f1f621d8e46aa4047be121494c7a42
David O. Kuda <david@kuda.ai>
2024-06-02 13:33:09 +0200

    avoid using MinInt

--- algorithms/dynamic_programming/rod_cutting.go
+++ algorithms/dynamic_programming/rod_cutting.go
@@ -1,17 +1,13 @@
 package dp
 
-const MaxUint = ^uint(0)
-const MinUint = 0
-const MaxInt = int(MaxUint >> 1)
-const MinInt = -MaxInt - 1
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // 1: Naive approach with a for loop
 func CutRodForLoop(p []int, n int) int {
 	if n == 0 {
 		return 0
 	}
-	q := MinInt
+	var q int
+	// the textbook uses: q := MinInt
 	for i := 1; i <= n; i++ {
 		q = max(q, p[i]+CutRodForLoop(p, n-i))
 	}
@@ -24,7 +20,8 @@ func CutRodRecursionOnly(p []int, n int) int {
 	if n == 0 {
 		return 0
 	}
-	q := MinInt
+	var q int
+	// the textbook uses: q := MinInt
 	q = cr(p, n, q, 1)
 	return q
 }
